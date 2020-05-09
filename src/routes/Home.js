e2f7078a9b55417548e30e98f899666bcfbf1888
qaws7791 <qaws7791@gmail.com>
2020-05-09 12:47:05 +0900

    cat->todo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,37 +3,38 @@ import { gql } from "apollo-boost";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import styled from "styled-components";
 
-const GET_CATS = gql`
+const GET_TODO = gql`
  query {
-     cats {
+     todos {
          id
          name
+         boolean
      }
  }
 `;
 
 // ADD_Cat은 mutation이름이 아니라 그냥 wrapping하는것 
 // 여기서 variables에서 받아오는 정보의 타입을 정의
-const ADD_CAT = gql`
-    mutation ADD_Cat($name: String!){ 
-	createCat(name: $name) {
-    id
-    name
-  }
-}
-`;
+// const ADD_CAT = gql`
+//     mutation ADD_Cat($name: String!){ 
+// 	createCat(name: $name) {
+//     id
+//     name
+//   }
+// }
+// `;
 
 export default () => {
 
     
     //const [addCat, {data}] = useMutation(ADD_CAT);
-     const { loading, data } = useQuery(GET_CATS);
+     const { loading, data } = useQuery(GET_TODO;
 
     return (
         <div>
         {loading && <h1>loading...</h1>}
         <h1>name</h1>
-        {data?.cats?.map( c => (
+        {data?.todos?.map( c => (
             <p> {c.name}</p>
         ))}
         {/* <button onClick={e => {
